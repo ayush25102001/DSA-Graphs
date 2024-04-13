2cc4cde81a2d3c495df0894a13eb0dd5beba8e8d
Ayush Kangane <78008646+ayush25102001@users.noreply.github.com>
2024-04-13 22:47:21 +0530

    Update Graph-11(Dijkstra'sShortestPathAlgo).cpp

--- Graph-11(Dijkstra'sShortestPathAlgo).cpp
+++ Graph-11(Dijkstra'sShortestPathAlgo).cpp
@@ -1,8 +1,3 @@
-// { Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
- // } Driver Code Ends
 class Solution
 {
 	public:
@@ -15,11 +10,16 @@ class Solution
         distTo[s]=0;
         priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > > pq;
         pq.push({0,s});
+        vector<int> vis(v,0);
         while(!pq.empty()) {
             auto x=pq.top();
             int dis=x.first;
             int node=x.second;
             pq.pop();
+    	    if(vis[node] == 1) {
+    		    continue;
+    	    }
+    	    vis[node] = 1;
             for(auto it:adj[node]){     
                 int dis_it=it[1];
                 int n_it=it[0];
@@ -32,43 +32,3 @@ class Solution
         return distTo;
     }
 };
-
-
-// { Driver Code Starts.
-
-
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> adj[V];
-        int i=0;
-        while (i++<E) {
-            int u, v, w;
-            cin >> u >> v >> w;
-            vector<int> t1,t2;
-            t1.push_back(v);
-            t1.push_back(w);
-            adj[u].push_back(t1);
-            t2.push_back(u);
-            t2.push_back(w);
-            adj[v].push_back(t2);
-        }
-        int S;
-        cin>>S;
-        
-        Solution obj;
-    	vector<int> res = obj.dijkstra(V, adj, S);
-    	
-    	for(int i=0; i<V; i++)
-    	    cout<<res[i]<<" ";
-    	cout<<endl;
-    }
-
-    return 0;
-}
-
-  // } Driver Code Ends
